f1e83a249d5dcc5c924af94f261b09207aec4383
U-PC-201603102054\Administrator <haiwei.free@gmail.com>
2016-06-17 17:54:43 +0800

    Admin: add

---
+++ Application/Common/Conf/debug.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'DB_TYPE'=>'mysql',
+    'DB_HOST'=>'192.168.99.100',
+    'DB_NAME'=>'dwz_thinkphp',
+    'DB_USER'=>'root',
+    'DB_PWD'=>'a123456',
+    'DB_PORT'=>'3306',
+    'DB_PREFIX'=>'',
+];
